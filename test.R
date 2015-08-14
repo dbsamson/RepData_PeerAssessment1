3d7e99f893281b7a7e20ba8b4291f9c5ced52fda
David Samson <davidsamson.efrat@gmail.com>
2015-08-14 13:54:36 +0300

    Finishing Touches

--- test.R
+++ test.R
@@ -1,9 +1,15 @@
-z<-matrix(rnorm(30),10,3,dimnames=list(c(),c("a","b","c")))
-z[3,3]<-NA
-z[10,3]<-NA
-head(z)
-vals<-matrix(c(1,2,3,4,5,6,7,8,9,10),10,1)
-for(i in 1:nrow(z)) {
-  print(i)
-  if (is.na(z[i,3])) {z[i,3]<- vals[i]}
-}
+raw$weekend<- (weekdays(ymd(as.character(raw$date))) %in% c("Saturday","Sunday"))
+weekdaysData <- raw[!raw$weekend,]
+weekendsData <- raw[raw$weekend,]
+# par(mfrow = c(2, 1))
+layout(matrix(c(1,1,2,2), 2,2, byrow = TRUE))
+
+# Weekdays first 
+weekdaysByInterval<-group_by(weekdaysData,interval)
+weekdaystotalsByInterval <- summarise(weekdaysByInterval,steps=mean(steps,na.rm=T))
+plot(x=weekdaysByInterval$interval,y=weekdaysByInterval$steps,xlab="Interval Nbr",ylab="Steps",main="Weekdays: Average Number of Steps per Interval",type="l")
+
+# Weekends  
+weekendsByInterval<-group_by(weekendsData,interval)
+weekendstotalsByInterval <- summarise(weekendsByInterval,steps=mean(steps,na.rm=T))
+plot(x=weekendsByInterval$interval,y=weekendsByInterval$steps,xlab="Interval Nbr",ylab="Steps",main="Weekends: Average Number of Steps per Interval",type="l")
